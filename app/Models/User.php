2b4821563689154e76af6d60bae5a2c70219f60d
itsmesaykou <itsmesaykou@gmail.com>
2026-03-17 22:27:15 +0700

    Fix Session Migration User Error

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\ActivityLog;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
